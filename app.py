9d133e4c09cbf8a366946c84d45c84b0106b256b
Lakshman S <slakshman664@gmail.com>
2022-05-23 19:43:14 +0530

    v2

--- app.py
+++ app.py
@@ -88,6 +88,13 @@ def getsize():
     print(list)
     return list
 
+@app.route("/",methods=['GET', "POST"])
+def index():
+    return render_template("index.html")
+
+@app.route("/results",methods=['GET', "POST"])
+def results():
+    return render_template("results.html")
 
 @app.route("/forumins",methods=['POST'])
 def forumins():
